fbcc2d557af95c0a6beda43d8b64a65eca483a04
Emile <emileswanepoel54@gmail.com>
2023-11-13 11:51:12 +0200

    Added docstrings to models.py

--- bandio/models.py
+++ bandio/models.py
@@ -1,16 +1,16 @@
+# models.py
 from django.db import models
 
-# Define your Band model which inherits from the 'models.Model' class.
 class Band(models.Model):
-    # Define a field to store the name of the band. It's a character field with a maximum length of 100 characters.
-    name = models.CharField(max_length=100)
-    
-    # Define a field to store the description of the band. It's a text field for longer descriptions.
-    description = models.TextField()
-    
-    # Define a field to store the number of members in the band. It's an integer field.
-    members = models.IntegerField()
+    """
+    Model representing a music band.
+    """
+    name = models.CharField(max_length=100, help_text="Enter the name of the band.")
+    description = models.TextField(help_text="Enter a description for the band.")
+    members = models.IntegerField(help_text="Enter the number of members in the band.")
 
-    # Define a human-readable representation of the model. This will be used in the Django admin interface.
     def __str__(self):
-        return self.name  # Return the name of the band as its string representation.
+        """
+        String representation of the Band object.
+        """
+        return self.name
