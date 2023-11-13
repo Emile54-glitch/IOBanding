d727c1a00ddcdb1522abe0d0286ef234876c8363
Emile <emileswanepoel54@gmail.com>
2023-11-13 11:51:36 +0200

    Added docstrings to views.py

--- bandio/views.py
+++ bandio/views.py
@@ -1,32 +1,43 @@
+# views.py
 from django.shortcuts import render
 from .models import Band
-from django.contrib.auth.forms import UserCreationForm  # Import Django's UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 
-# Create your views here.
-
 def home(request):
-    # Retrieve all bands from the database and pass them to the 'home.html' template
+    """
+    View for the home page.
+    Retrieves all bands from the database and passes them to the 'home.html' template.
+    """
     bands = Band.objects.all()
     return render(request, 'home.html', {'bands': bands})
 
 def about(request):
-    # Render the 'about.html' template
+    """
+    View for the about page.
+    Renders the 'about.html' template.
+    """
     return render(request, 'about.html')
 
 def contact(request):
-    # Render the 'contact.html' template
+    """
+    View for the contact page.
+    Renders the 'contact.html' template.
+    """
     return render(request, 'contact.html')
 
 def registration(request):
-    # Handle user registration using Django's UserCreationForm
+    """
+    View for user registration.
+    Handles user registration using Django's UserCreationForm.
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log the user in after successful registration
-            return redirect('home')  # Redirect to the home page after registration
+            login(request, user)
+            return redirect('home')
     else:
-        form = UserCreationForm()  # Create a new UserCreationForm for GET requests
+        form = UserCreationForm()
     return render(request, 'registration/registration.html', {'form': form})
