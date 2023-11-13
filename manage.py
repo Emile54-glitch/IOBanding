97a04822bf0a450b05e0ba9ce4a1db6dfe94b218
Emile <emileswanepoel54@gmail.com>
2023-11-13 11:52:21 +0200

    Added docstrings to manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
+# manage.py
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""
+Django's command-line utility for administrative tasks.
+"""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """
+    Run administrative tasks.
+    """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IOBanding.settings')
     try:
         from django.core.management import execute_from_command_line
