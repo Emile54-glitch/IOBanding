b7b407dd9a350b1cd360c0b81febffb5383c85a9
Emile <emileswanepoel54@gmail.com>
2023-11-14 18:11:08 +0200

    Add docstrings to models.py

--- bandio/models.py
+++ bandio/models.py
@@ -1,16 +1,21 @@
-# models.py
 from django.db import models
 
 class Band(models.Model):
     """
-    Model representing a music band.
+    Represents a music band.
+
+    Attributes:
+        name (str): The name of the band.
+        description (str): A description of the band.
+        members (int): The number of members in the band.
     """
+
     name = models.CharField(max_length=100, help_text="Enter the name of the band.")
     description = models.TextField(help_text="Enter a description for the band.")
     members = models.IntegerField(help_text="Enter the number of members in the band.")
 
     def __str__(self):
         """
-        String representation of the Band object.
+        Returns a string representation of the Band object.
         """
         return self.name
